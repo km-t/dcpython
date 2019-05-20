caf0964c8d24a37283e4b235a895851b086d4c85
km-t <km-t-1014-fms@docomo.ne.jp>
2019-05-20 13:10:33 +0900

    a

--- digital-curling/train_keras_v2.py
+++ digital-curling/train_keras_v2.py
@@ -13,39 +13,41 @@ import matplotlib.pyplot as plt
 
 
 def culcAccuracy(model, testx, y):
-    pre = np.empty((0,30), dtype = np.float32)
+    pre = np.empty((0,1), dtype = np.float32)
     for x in tqdm(testx):
         x = np.array([x], dtype=np.float32)
-        arg = np.argmax(model.predict(x))
-        p = np.zeros(30, dtype=np.float32)
-        p[arg] = 1
+        pp = round(float(model.predict(x)))
+        p = np.zeros(1, dtype=np.float32)
+        p[0] = pp
         pre = np.append(pre, [p], axis=0)
     ans = y
     count = 0
     true = 0
-    for i in range(len(pre)):    
-        isEqual = True
-        for j in range(len(pre[0])):
-            if pre[i][j]==ans[i][j]:
-                pass
-            else:
-                isEqual = False
-        if isEqual:
-            true += 1
-        count += 1
+    squaredCount = 0
+    acc = []
+    for i in range(len(pre)):
+        squaredCount += 1 - ((pre[i] - ans[i])**2)/4    
+        if pre[i]==ans[i]:
+            true+=1
+        count+=1
+        if i%int(len(pre)/epochs)==0:
+            acc.append(true/count)
     print(count, true, true/count)
+    print(squaredCount/count)
+    print(len(acc))
+    print(len(pre),int(len(pre)/epochs))
+    return acc
+    
 
 
 def getData(df):
-    x = np.empty((0, 43), dtype=np.float32)
-    y = np.empty((0,30), dtype=np.float32)
+    x = np.empty((0, 73), dtype=np.float32)
+    y = np.empty((0,1), dtype=np.float32)
     for i in range(len(df)):
         vec = str(df.iloc[i, 0])
-        v = np.zeros(43, dtype=np.float32)
+        v = np.zeros(73, dtype=np.float32)
         for j in range(43):
             v[j] = float(vec[j])
-        IN = np.array([v], dtype=np.float32)
-        o = np.zeros(30, dtype=np.float32)
         where = int(df.iloc[i,1])
         angle = int(df.iloc[i,2])
         p = int(df.iloc[i,3])
@@ -55,8 +57,14 @@ def getData(df):
             if pData[k]==p:
                 power = k
         ans = power*6+angle*3+where
-        #o[ans] = int(df.iloc[i,4])
-        o[ans] = 1
+        for j in range(30):
+            if j==ans:
+                v[j]=1
+            else:
+                v[j]=0
+        IN = np.array([v], dtype=np.float32)
+        o = np.zeros(1, dtype=np.float32)
+        o[0] = int(df.iloc[i,4])
         OUT = np.array([o], dtype=np.float32)
         x = np.append(x, IN, axis=0)
         y = np.append(y, OUT, axis=0)
@@ -66,8 +74,8 @@ def getDF(df_origin):
     trainNum = int(len(df_origin)*DataSize)
     testNum = int((len(df_origin)-trainNum)*DataSize)
     df_origin=df_origin.sample(frac=1)
-    df_train = df_origin[:trainNum]
-    df_test = df_origin[trainNum+1: trainNum+testNum]
+    df_train = df_origin[:trainNum].sample(frac=1)
+    df_test = df_origin[trainNum+1: trainNum+testNum].sample(frac=1)
     return df_train, df_test
 
 
@@ -75,13 +83,13 @@ def train(x, y):
     # ネットワーク定義
     model = Sequential()
     activations = ['relu','elu','selu','softplus','softsign','tanh','sigmoid','hard_sigmoid','linear','softmax']
-    activation = activations[6]#0~8
-    model.add(Dense(256, input_dim=43, activation=activation))
+    activation = activations[8]#0~9
+    model.add(Dense(256, input_dim=73, activation=activation))
     model.add(Dense(128, activation=activation))
     model.add(Dense(256, activation=activation))
     model.add(Dense(128, activation=activation))
-    model.add(Dense(30, activation=activation))
-    model.compile(loss='categorical_crossentropy',
+    model.add(Dense(1, activation=activation))
+    model.compile(loss='mean_squared_error',
                 optimizer='Adam',
                 metrics=['accuracy'])
     history = model.fit(x, y,
@@ -90,13 +98,14 @@ def train(x, y):
     return model, history
 
 def test(model, history, x, y):
-    culcAccuracy(model, x, y)
-    plt.plot(range(1, epochs+1), history.history['loss'], label="loss")
-    plt.plot(range(1, epochs+1), history.history['acc'], label="acc")
+    myAcc = culcAccuracy(model, x, y)
+    plt.plot(history.history['loss'], label="loss")
+    plt.plot(history.history['acc'], label="acc")
+    plt.plot(myAcc, label = 'myAcc')
     plt.xlabel('Epochs')
     plt.ylabel('')
     plt.legend()
-    #plt.show()
+    plt.show()
 
 
 def main():
@@ -105,20 +114,20 @@ def main():
     x_train, y_train = getData(df_train)
     print("get test data")
     x_test, y_test = getData(df_test)
-
+    print(y_train)
     print('strat train')
     model, history = train(x_train, y_train)
+    
 
     test(model, history, x_test, y_test)
 
-
 if __name__ == "__main__":
-    DataSize = 0.1
-    batch_size=32
-    epochs=50
+    DataSize = 0.01
+    batch_size=512
+    epochs=4000
     df = pd.read_csv('logs.csv',sep=',',header=None,names=('vector','where','angle','power','reward'))
-    df = df[df['reward']>=2]
-    print(df)
+    #df = df[(df['vector']=='1000010000000000001000000010000000000000000')]
+    #df = df[df['reward']>=2]
     print(len(df))
     main()
 
